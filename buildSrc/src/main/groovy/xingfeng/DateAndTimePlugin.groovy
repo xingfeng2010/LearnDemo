942e6f1d2c82041c90b393d8dfa5946f82422045
李世星 <70449@any3.com>
2019-09-29 09:37:40 +0800

    自定义插件2

--- buildSrc/src/main/groovy/xingfeng/DateAndTimePlugin.groovy
+++ buildSrc/src/main/groovy/xingfeng/DateAndTimePlugin.groovy
@@ -3,7 +3,7 @@ package xingfeng
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-class DateAndtimePlugin implements Plugin<Project> {
+class DateAndTimePlugin implements Plugin<Project> {
 
     void apply(Project project) {
         project.extensions.create("dateAndTime", DateAndTimePluginExtension)
