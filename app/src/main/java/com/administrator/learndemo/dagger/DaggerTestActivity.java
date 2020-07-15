9b419d281f494ea232523f97b61a723c822d4bc1
李世星 <70449@any3.com>
2020-07-15 17:00:43 +0800

    update

--- app/src/main/java/com/administrator/learndemo/dagger/DaggerTestActivity.java
+++ app/src/main/java/com/administrator/learndemo/dagger/DaggerTestActivity.java
@@ -15,6 +15,7 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
@@ -36,6 +37,7 @@ import io.reactivex.schedulers.Schedulers;
 import okhttp3.ResponseBody;
 
 public class DaggerTestActivity extends AppCompatActivity implements ICommonView {
+    private static final String TAG = "RXJAVA";
 
     @BindView(R.id.btn_login)
     Button btn;
@@ -61,8 +63,29 @@ public class DaggerTestActivity extends AppCompatActivity implements ICommonView
         testRxjava();
 
         testMap();
+
+        testJustBuffer();
     }
 
+    private void testJustBuffer() {
+        Observable.just("test1","test2","test3","test4","test5","test6").buffer(2)
+                .subscribe(new Consumer<List<String>>() {
+                    @Override
+                    public void accept(List<String> strings) throws Exception {
+                        Log.e(TAG, "SINGLE ARRAY");
+                        for (String str: strings) {
+                            Log.e(TAG, "testJustBuffer:" + str);
+                        }
+                    }
+                });
+    }
+
+    /**
+     * 两者都可以实现数据集合中一对多事件的转换，后者会按发送的顺序获取接收结果，前者可能是乱序接收（不确定哪个事件先完成）。
+     *
+     * 一对多事件转换：在flatMap集合中例如可以操作一个公司实体，并转换为单个部门实体，返回后在后续的accept中，
+     * 又可以使用单个部门实体对每个成员进行逻辑处理。
+     */
     private void testMap() {
         Observable.fromArray(1, 2, 3, 4, 5)
                 .flatMap(new Function<Integer, ObservableSource<Integer>>() {
@@ -81,7 +104,28 @@ public class DaggerTestActivity extends AppCompatActivity implements ICommonView
                 .subscribe(new Consumer<Integer>() {
                     @Override
                     public void accept(@NonNull Integer integer) throws Exception {
-                        Log.e("tag", "accept:" + integer);
+                        Log.e(TAG, "flatMap:" + integer);
+                    }
+                });
+
+        Observable.fromArray(1, 2, 3, 4, 5)
+                .concatMap(new Function<Integer, ObservableSource<Integer>>() {
+                    @Override
+                    public ObservableSource<Integer> apply(@NonNull Integer integer) throws Exception {
+
+                        int delay = 0;
+                        if (integer == 3) {
+                            delay = 500;//延迟500ms
+                        }
+                        return Observable.just(integer * 10).delay(delay, TimeUnit.MILLISECONDS);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        Log.e(TAG, "concatMap:" + integer);
                     }
                 });
     }
@@ -99,7 +143,7 @@ public class DaggerTestActivity extends AppCompatActivity implements ICommonView
                     return student.getName();
                 })
                 .subscribe((String s) -> {
-                    Log.i("DEBUG_TEST", "testUserInfo:" + s);
+                    Log.i(TAG, "testUserInfo:" + s);
                 });
     }
 
